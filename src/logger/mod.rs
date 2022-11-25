60b9bc72d4a1bf41b36cd1af83adcb47d5ee6b30
Areskul <areskul@areskul.com>
2022-11-25 13:14:29 +0100

    working module

--- src/logger/mod.rs
+++ src/logger/mod.rs
@@ -1,29 +1,56 @@
-use log::LevelFilter;
-use log::{debug, error, info, trace, warn};
-use log4rs::append::console::ConsoleAppender;
-use log4rs::append::file::FileAppender;
-use log4rs::config::{Appender, Config, Logger, Root};
-use log4rs::encode::pattern::PatternEncoder;
+use log::{debug, error, info, trace, warn, LevelFilter, SetLoggerError};
+use log4rs::{
+    append::{
+        console::{ConsoleAppender, Target},
+        file::FileAppender,
+    },
+    config::{Appender, Config, Root},
+    encode::pattern::PatternEncoder,
+    filter::threshold::ThresholdFilter,
+};
 
-fn main() {
-    let stdout = ConsoleAppender::builder().build();
-    let requests = FileAppender::builder()
-        .encoder(Box::new(PatternEncoder::new("{d} - {m}{n}")))
-        .build("log/requests.log")
+pub fn set_logger_config() -> Result<(), SetLoggerError> {
+    let level = log::LevelFilter::Info;
+    let file_path = "./logs";
+
+    // Build a stderr logger.
+    let stderr = ConsoleAppender::builder().target(Target::Stderr).build();
+
+    // Logging to log file.
+    let logfile = FileAppender::builder()
+        // Pattern: https://docs.rs/log4rs/*/log4rs/encode/pattern/index.html
+        .encoder(Box::new(PatternEncoder::new("{l} - {m}\n")))
+        .build(file_path)
         .unwrap();
 
+    // Log Trace level output to file where trace is the default level
+    // and the programmatically specified level to stderr.
     let config = Config::builder()
-        .appender(Appender::builder().build("stdout", Box::new(stdout)))
-        .appender(Appender::builder().build("requests", Box::new(requests)))
-        .logger(Logger::builder().build("app::backend::db", LevelFilter::Info))
-        .logger(
-            Logger::builder()
-                .appender("requests")
-                .additive(false)
-                .build("app::requests", LevelFilter::Info),
+        .appender(Appender::builder().build("logfile", Box::new(logfile)))
+        .appender(
+            Appender::builder()
+                .filter(Box::new(ThresholdFilter::new(level)))
+                .build("stderr", Box::new(stderr)),
         )
-        .build(Root::builder().appender("stdout").build(LevelFilter::Warn))
+        .build(
+            Root::builder()
+                .appender("logfile")
+                .appender("stderr")
+                .build(LevelFilter::Trace),
+        )
         .unwrap();
 
-    let handle = log4rs::init_config(config).unwrap();
+    // Use this to change log levels at runtime.
+    // This means you can change the default log level to trace
+    // if you are trying to debug an issue and need more logs on then turn it off
+    // once you are done.
+    let _handle = log4rs::init_config(config)?;
+
+    error!("Goes to stderr and file");
+    warn!("Goes to stderr and file");
+    info!("Goes to stderr and file");
+    debug!("Goes to file only");
+    trace!("Goes to file only");
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,10 @@
-use log::LevelFilter;
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+
+mod logger;
 use log::{debug, error, info, trace, warn};
-use log4rs::append::console::ConsoleAppender;
-use log4rs::append::file::FileAppender;
-use log4rs::config::{Appender, Config, Logger, Root};
-use log4rs::encode::pattern::PatternEncoder;
 
 fn main() {
-    let stdout = ConsoleAppender::builder().build();
-
-    let requests = FileAppender::builder()
-        .encoder(Box::new(PatternEncoder::new("{d} - {m}{n}")))
-        .build("log/requests.log")
-        .unwrap();
-
-    let config = Config::builder()
-        .appender(Appender::builder().build("stdout", Box::new(stdout)))
-        .appender(Appender::builder().build("requests", Box::new(requests)))
-        .logger(Logger::builder().build("app::backend::db", LevelFilter::Info))
-        .logger(
-            Logger::builder()
-                .appender("requests")
-                .additive(false)
-                .build("app::requests", LevelFilter::Info),
-        )
-        .build(Root::builder().appender("stdout").build(LevelFilter::Warn))
-        .unwrap();
-
-    let handle = log4rs::init_config(config).unwrap();
-
-    // use handle to change logger configuration at runtime
-    debug!("this is a debug {}", "message");
+    logger::set_logger_config();
+    info!("this is a debug");
 }
-// fn main (){
-// }
