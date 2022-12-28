a50848dcdf1c30a288ac6a5c99cb477fa5aec82b
Areskul <areskul@areskul.com>
2022-12-28 17:34:51 +0100

    bulk restructure

--- pipelight/src/bin/pipelight-run.rs
+++ pipelight/src/bin/pipelight-run.rs
@@ -5,8 +5,8 @@ use log::LevelFilter::{Debug, Trace};
 #[allow(dead_code)]
 use shared::config::get_pipeline;
 use shared::logger::{debug, error, info, set_logger, trace, warn};
-use shared::types::config::{Pipeline, Step};
 use shared::types::logs::{PipelineLog, PipelineStatus, StepLog};
+use shared::types::{Pipeline, Step};
 use std::env;
 use std::error::Error;
 use std::process::exit;
@@ -23,7 +23,7 @@ fn handler() -> Result<(), Box<dyn Error>> {
     let handle = set_logger(Trace)?;
     let args = env::args().collect::<Vec<String>>();
     let pipeline_name: String = args[1].to_owned();
-    let p: Pipeline = get_pipeline(&pipeline_name)?;
+    let p: Pipeline = Config::get()?.pipelines.get(&pipeline_name)?;
     let mut pipeline = PipelineLog::from(p);
     pipeline.run(handle);
     Ok(())
--- pipelight/src/main.rs
+++ pipelight/src/main.rs
@@ -1,13 +1,11 @@
 #![allow(unused_variables)]
-#![allow(unused_imports)]
+// #![allow(unused_imports)]
 #![allow(unused_must_use)]
 // use performance::*;
 use log::{debug, error, info, trace, warn};
 use shared::cli;
 use shared::exec;
 #[allow(dead_code)]
-use shared::logger;
-use shared::types::config::Config;
 use std::error::Error;
 use std::process::exit;
 
