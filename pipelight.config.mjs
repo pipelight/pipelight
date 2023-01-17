3ac405dbecdfbfd36e459e4b7c0a3e8efcf1e183
Areskul <areskul@areskul.com>
2023-01-17 15:37:18 +0100

    add new test pipeline

--- pipelight.config.mjs
+++ pipelight.config.mjs
@@ -1,6 +1,21 @@
 const config = {
   pipelines: [
     {
+      name: "default",
+      steps: [
+        {
+          name: "wait",
+          commands: ["ls", "sleep 30", "pwd"],
+        },
+      ],
+      triggers: [
+        {
+          branches: ["master", "dev"],
+          actions: ["pre-push"],
+        },
+      ],
+    },
+    {
       name: "test",
       steps: [
         {
