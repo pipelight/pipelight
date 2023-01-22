8fa634208cd2f3a341394518b74ad9b5f56e9440
Areskul <areskul@areskul.com>
2023-01-22 17:42:32 +0100

    ls pipelines and safe fallback if no logs

--- shared/src/cli/print.rs
+++ shared/src/cli/print.rs
@@ -36,25 +36,23 @@ pub fn list() -> Result<(), Box<dyn Error>> {
         "status", "date" ,"name"
     );
     for pipeline in &config.pipelines.unwrap() {
-        let pipe_logs = Logs::get_by_name(&pipeline.name)?;
-        let pipe_last_log = pipe_logs.iter().next().unwrap().clone();
-        let date;
-        if pipe_last_log.status.is_some() {
-            let str_date = pipe_last_log.event.unwrap().date;
-            date = str_date
-                .parse::<DateTime<Local>>()
-                .unwrap()
-                .format("%Y-%m-%d %H:%M:%S")
-                .to_string();
-        } else {
-            date = "".to_owned();
+        let mut date = "".to_owned();
+        let mut status = "".to_owned();
+        // Retrieve logs data if any
+        if Logs::get().is_ok() {
+            let pipe_logs = Logs::get_by_name(&pipeline.name)?;
+            let pipe_last_log = pipe_logs.iter().next().unwrap().clone();
+            if pipe_last_log.status.is_some() {
+                status = String::from(&pipe_last_log.status.unwrap());
+                let str_date = pipe_last_log.event.unwrap().date;
+                date = str_date
+                    .parse::<DateTime<Local>>()
+                    .unwrap()
+                    .format("%Y-%m-%d %H:%M:%S")
+                    .to_string();
+            }
         }
-        println!(
-            "{:<10} {:<25} {:<40}",
-            String::from(&pipe_last_log.status.unwrap()),
-            date,
-            pipeline.name
-        );
+        println!("{:<10} {:<25} {:<40}", status, date, pipeline.name);
     }
     Ok(())
 }
