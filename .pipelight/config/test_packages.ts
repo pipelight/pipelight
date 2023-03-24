d195b013ec1aef70dae47eba495a6c48b1b542fd
Areskul <areskul@areskul.com>
2023-03-24 09:44:47 +0100

    add pipelines: package creating and testing through docker

--- .pipelight/config/test_packages.ts
+++ .pipelight/config/test_packages.ts
@@ -1,37 +1,61 @@
 import type { Config, Pipeline } from "npm:pipelight";
 
 const params = {
-  distros: ["debian", "archlinux", "fedora", "centos"],
+  distros: [
+    // {
+    //   name: "debian",
+    //   prefix: "deb",
+    //   archive: "deb",
+    // },
+    {
+      name: "archlinux",
+      prefix: "aur",
+      archive: "pkg.tar.zst",
+    },
+    {
+      name: "fedora",
+      prefix: "rpm",
+      archive: "rpm",
+    },
+  ],
 };
 
-const makePipeline = ({ distros }: any): Pipeline => ({
-  name: "test:packages",
-  steps: [
-    {
-      name: "clean",
-      commands: [`docker container rm debian.latest `],
-      non_blocking: true,
-    },
-    {
-      name: "build container",
-      commands: [
-        `sh -c \
+const makePipeline = ({ distros }: any): Pipeline => {
+  let pipeline: Pipeline = {
+    name: "test:packages",
+    steps: [],
+  };
+
+  for (const { name, prefix } of distros) {
+    pipeline.steps.push(
+      {
+        name: "clean old container",
+        commands: [`docker container rm ${name}.latest `],
+        non_blocking: true,
+      },
+      {
+        name: "build container",
+        commands: [
+          `sh -c \
           "cd ../ && docker buildx build \
-          -f pipelight/.pipelight/docker/Dockerfile.debian \
-          -t debian.latest ."`,
-      ],
-    },
-    {
-      name: "run container",
-      commands: [
-        `docker run \
+            -f pipelight/.pipelight/docker/Dockerfile.${prefix} \
+            -t ${name}.latest ."`,
+        ],
+      },
+      {
+        name: "run container",
+        commands: [
+          `docker run \
+          --mount type=bind,source=/packages,target=/root/dist \
           --name="debian.latest" \
           debian.latest
         `,
-      ],
-    },
-  ],
-});
-const testPackagingPipeline = makePipeline(params);
+        ],
+      }
+    );
+  }
+  return pipeline;
+};
 
+const testPackagingPipeline = makePipeline(params);
 export { testPackagingPipeline };
