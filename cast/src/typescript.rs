90a7683f2d31d4ecbe6764a025a22ef4412c1685
Areska <areskul@areskul.com>
2023-07-27 12:18:42 +0200

    disable inner console logs

--- cast/src/typescript.rs
+++ cast/src/typescript.rs
@@ -1,20 +1,37 @@
 pub fn main_script(file_name: &str) -> String {
     format!(
         r#"'
-    const stock = console;
-    console = {{}};
-    const cwd = Deno.cwd();
-    const promess = import(`{}`);
-    promess
-      .then((res) => {{
-        const config = res.default;
-        const json = JSON.stringify(config, null, 2);
-        console = stock;
-        console.log(json);
-      }})
-      .catch((err) => {{
-        console.log(err);
-      }});
+// Disable console log statements
+const {{ log, error, warn, info, debug, trace }} = console;
+const backup = {{ log, error, warn, info, debug, trace }};
+
+// Disable console
+const disable = function () {{
+  for (let [key, value] of Object.entries(console)) {{
+    console[key] = () => undefined;
+  }}
+}};
+
+// Enable console
+const enable = function () {{
+  console = backup;
+}};
+
+// Load config file and mutate log statements
+const promess = import(`{}`);
+disable();
+promess
+  .then((res) => {{
+    const config = res.default;
+    const json = JSON.stringify(config, null, 2);
+
+    enable();
+    console.log(json);
+  }})
+  .catch((err) => {{
+    enable();
+    console.error(err);
+  }});
     '"#,
         file_name
     )
