ec8f8014c7aacbef8e2fe2baa7e0619bd0f404ab
areskill <areskul@areskul.com>
2024-08-05 11:49:23 +0200

    rename nix file

--- flake.nix
+++ flake.nix
@@ -5,21 +5,37 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs = {
+    self,
     nixpkgs,
     rust-overlay,
     flake-utils,
-    self,
+    flake-parts,
   } @ inputs:
-    with inputs;
-      flake-utils.lib.eachDefaultSystem (
-        system: let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in rec {
-          packages.default = pkgs.callPackage ./default.nix {};
-          devShells.default = pkgs.callPackage ./shell.nix {};
-        }
-      );
+    flake-parts.lib.mkFlake {
+      inherit inputs;
+    } {
+      flake = {
+        nixosModules = {
+          jucenit = ./module.nix;
+        };
+      };
+
+      systems =
+        flake-utils.lib.allSystems;
+      perSystem = {
+        config,
+        self,
+        inputs,
+        pkgs,
+        system,
+        ...
+      }: {
+        packages.default = pkgs.callPackage ./package.nix {};
+        devShells.default = pkgs.callPackage ./shell.nix {};
+      };
+    };
 }
