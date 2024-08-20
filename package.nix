d248a4f0c30cc3a3824a955edccadceb062da128
areskill <areskul@areskul.com>
2024-08-20 14:33:46 +0200

    fix(flake): install autocompletion scripts

--- package.nix
+++ package.nix
@@ -6,20 +6,10 @@
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "pipelight";
   version = "0.8.0";
-
   src = ./.;
-
   cargoLock = {
     lockFile = ./Cargo.lock;
   };
-  # cargoBuildHook = ''
-  # buildPhase = ''
-  #   cargo build --release
-  # '';
-  # installPhase = ''
-  #   mkdir -p $out/bin
-  #   install -t target/release/${pname} $out/bin
-  # '';
   # disable tests
   checkType = "debug";
   doCheck = false;
@@ -33,10 +23,9 @@ pkgs.rustPlatform.buildRustPackage rec {
   ];
   PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
 
-  postInstall = with lib; ''
-    installShellCompletion --cmd ${pname}\
-      --bash ./autocompletion/${pname}.bash \
-      --fish ./autocompletion/${pname}.fish \
-      --zsh  ./autocompletion/_${pname}
+  postInstall = ''
+    installShellCompletion --bash ./autocompletion/${pname}.bash
+    installShellCompletion --fish ./autocompletion/${pname}.fish
+    installShellCompletion --zsh  ./autocompletion/_${pname}
   '';
 }
